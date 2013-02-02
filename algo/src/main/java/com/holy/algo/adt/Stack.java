7089fe0d857b2831d928997d44353b1c1e315034
Alan S. Li <toro.devel@gmail.com>
2013-02-02 09:01:32 -0800

    initial creation

---
+++ algo/src/main/java/com/holy/algo/adt/Stack.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.holy.algo.adt;
+
+import java.util.Iterator;
+
+/**
+ * @author ali
+ *
+ */
+public class Stack<T> implements Iterable<T> {
+
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ algo/src/test/java/com/holy/algo/adt/TestStack.java
@@ -0,0 +1,11 @@
+package com.holy.algo.adt;
+
+import org.testng.annotations.Test;
+
+public class TestStack {
+
+	@Test
+	public void testStringStack(){
+		
+	}
+}
