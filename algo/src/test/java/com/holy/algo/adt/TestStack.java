6c593140bb1ea3ac4745d56c2ab1ca2a6450d5d8
Alan S. Li <toro.devel@gmail.com>
2013-02-02 11:14:02 -0800

    added test case one

--- algo/src/test/java/com/holy/algo/adt/TestStack.java
+++ algo/src/test/java/com/holy/algo/adt/TestStack.java
@@ -6,6 +6,9 @@ public class TestStack {
 
 	@Test
 	public void testStringStack(){
+		Stack<String> stringStack = new Stack<String>();
+		stringStack.push("one");
+		System.out.println("size of stringStack:" + stringStack.size());
 		System.out.println("this is a testStringStack test");
 	}
 }
